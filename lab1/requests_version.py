ec01c98c375d63ba6836f66697625107714c36a0
Gerrinator <vangenderengerard@gmail.com>
2023-01-09 18:35:52 -0700

    Modified requests_version.py

--- lab1/requests_version.py
+++ lab1/requests_version.py
@@ -1,3 +1,4 @@
 import requests
-#print(requests.__version__)
-requests.get("http://www.google.com/")
+print(requests.__version__)
+print(requests.get("http://www.google.com/").content)
+print(requests.get("https://raw.githubusercontent.com/Gerrinator/404labs/master/lab1/requests_version.py").content)
